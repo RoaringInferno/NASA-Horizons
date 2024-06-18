90e145a606de92ff9d2bd243fb27b9279dbedfcf
RoaringInferno <jj2t17@gmail.com>
2024-06-19 00:02:30 +0900

    Made Horizon output file parser members private, shielded data
    Added size field to parser
    
    Moved specific field parsers into the parser's private fields

--- src/horizons/horizon_output_parsing.hpp
+++ src/horizons/horizon_output_parsing.hpp
@@ -14,92 +14,96 @@ struct HorizonEphemerisData
     EquatorialCoordinate equatorial;
 };
 
-/**
- * Date string in the format "YYYY-MON-DD HH:MM"
- */
-DateTime parseHorizonDateTime(const std::string& date_string)
-{
-    /**
-     * Jan
-     * Feb
-     * Mar
-     * Apr
-     * May
-     * Jun
-     * Jul
-     * Aug
-     * Sep
-     * Oct
-     * Nov
-     * Dec
-     */
-    const auto month_hash = [](const std::string& month) -> MONTH {
-        switch ((month[0] - 'A') * 4 + (month[1] - 'a')) {
-            case 29: return JANUARY; 
-            case 37: return FEBRUARY;
-            case 45: return MARCH; 
-            case 15: return APRIL; 
-            case 12: return MAY; 
-            case 43: return JUNE; 
-            case 44: return JULY; 
-            case 48: return AUGUST; 
-            case 30: return SEPTEMBER; 
-            case 24: return OCTOBER; 
-            case 49: return NOVEMBER; 
-            case 33: return DECEMBER; 
-            default: return ERR_MONTH;
-        }
-    };
 
-    DateTime date;
-
-    date.setYear(std::stoi(date_string.substr(0, 4)));
-    date.setDay(std::stoi(date_string.substr(8, 2)));
-    date.setHour(std::stoi(date_string.substr(11, 2)));
-    date.setMinute(std::stoi(date_string.substr(14, 2)));
-    date.setMonth(month_hash(date_string.substr(5, 3)));
-
-    return date;
-    
-}
-
-/**
- * RA in the format HH MM SS.ff
- */
-RightAscension parseHorizonRA(const std::string& ra_string)
-{
-    RightAscension ra;
-
-    ra.setDegrees(std::stoi(ra_string.substr(0, 2)) * 15);
-    ra.setMinutes(std::stoi(ra_string.substr(3, 2)));
-    ra.setSeconds(std::stod(ra_string.substr(6, 5)));
-    ra.setMilliseconds(std::stoi(ra_string.substr(9, 2)) * 10);
-
-    return ra;
-}
-
-/**
- * DEC in the format sDD MM SS.ff
- */
-Declination parseHorizonDEC(const std::string& dec_string)
-{
-    Declination dec;
-
-    dec.setDegrees(std::stoi(dec_string.substr(1, 2)) * ((dec_string[0] == '-') ? -1 : 1));
-    dec.setMinutes(std::stoi(dec_string.substr(4, 2)));
-    dec.setSeconds(std::stod(dec_string.substr(7, 5)));
-    dec.setMilliseconds(std::stoi(dec_string.substr(10, 2)) * 10);
-
-    return dec;
-}
 
 class ParsedHorizonGeneratedEphemeris
 {
-    HorizonEphemerisData* ephemeris_data;
-public:
-    ParsedHorizonGeneratedEphemeris(HorizonGeneratedEphemeris& generated_ephemeris) : ephemeris_data(new HorizonEphemerisData[generated_ephemeris.getRawEphemerisData().size()])
+    /**
+     * Date string in the format "YYYY-MON-DD HH:MM"
+     */
+    DateTime parseHorizonDateTime(const std::string& date_string)
     {
-        for (unsigned int i = 0; i < generated_ephemeris.getRawEphemerisData().size(); i++)
+        /**
+         * Jan
+         * Feb
+         * Mar
+         * Apr
+         * May
+         * Jun
+         * Jul
+         * Aug
+         * Sep
+         * Oct
+         * Nov
+         * Dec
+         */
+        const auto month_hash = [](const std::string& month) -> MONTH {
+            switch ((month[0] - 'A') * 4 + (month[1] - 'a')) {
+                case 29: return JANUARY; 
+                case 37: return FEBRUARY;
+                case 45: return MARCH; 
+                case 15: return APRIL; 
+                case 12: return MAY; 
+                case 43: return JUNE; 
+                case 44: return JULY; 
+                case 48: return AUGUST; 
+                case 30: return SEPTEMBER; 
+                case 24: return OCTOBER; 
+                case 49: return NOVEMBER; 
+                case 33: return DECEMBER; 
+                default: return ERR_MONTH;
+            }
+        };
+
+        DateTime date;
+
+        date.setYear(std::stoi(date_string.substr(0, 4)));
+        date.setDay(std::stoi(date_string.substr(8, 2)));
+        date.setHour(std::stoi(date_string.substr(11, 2)));
+        date.setMinute(std::stoi(date_string.substr(14, 2)));
+        date.setMonth(month_hash(date_string.substr(5, 3)));
+
+        return date;
+        
+    }
+
+    /**
+     * RA in the format HH MM SS.ff
+     */
+    RightAscension parseHorizonRA(const std::string& ra_string)
+    {
+        RightAscension ra;
+
+        ra.setDegrees(std::stoi(ra_string.substr(0, 2)) * 15);
+        ra.setMinutes(std::stoi(ra_string.substr(3, 2)));
+        ra.setSeconds(std::stod(ra_string.substr(6, 5)));
+        ra.setMilliseconds(std::stoi(ra_string.substr(9, 2)) * 10);
+
+        return ra;
+    }
+
+    /**
+     * DEC in the format sDD MM SS.ff
+     */
+    Declination parseHorizonDEC(const std::string& dec_string)
+    {
+        Declination dec;
+
+        dec.setDegrees(std::stoi(dec_string.substr(1, 2)) * ((dec_string[0] == '-') ? -1 : 1));
+        dec.setMinutes(std::stoi(dec_string.substr(4, 2)));
+        dec.setSeconds(std::stod(dec_string.substr(7, 5)));
+        dec.setMilliseconds(std::stoi(dec_string.substr(10, 2)) * 10);
+
+        return dec;
+    }
+
+    HorizonEphemerisData* ephemeris_data;
+
+    unsigned long size;
+public:
+    ParsedHorizonGeneratedEphemeris(HorizonGeneratedEphemeris& generated_ephemeris) : size(generated_ephemeris.getRawEphemerisData().size()), ephemeris_data(new HorizonEphemerisData[size])
+    {
+        for (unsigned int i = 0; i < size; i++)
         {
             std::string& line = generated_ephemeris.getRawEphemerisData()[i];
             // Parse date
@@ -114,6 +118,8 @@ public:
     }
 
     HorizonEphemerisData* getEphemerisData() const { return ephemeris_data; }
+
+    unsigned long getSize() const { return size; }
 };
 
 class HorizonGeneratedEphemeris
